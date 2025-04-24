479513922825c27f99abb645cfc18a1014c27b87
Gurjeevan <gurjeevanmalhi@outlook.com>
2025-04-23 21:38:05 -0500

    Added solution to question 4

--- Business Problems.sql
+++ Business Problems.sql
@@ -1,29 +1,5 @@
 -- Business Problems and Solutions
 
--- Notes
-select *
-from locations
-where city = 'Queens';
-
-select * 
-from trip_details;
-
-select 
-    t.*,
-    l.city as pickup_city,
-    l2.city as dropoff_city,
-    (t.fare_amount + t.surge_fee) as total_revenue
-from trip_details as t
-left join locations as l 
-    on t.pulocationid = l.locationid
-left join locations as l2
-    on t.dolocationid = l2.locationid;
-
-select surge_fee
-from trip_details
-order by surge_fee asc;
-
-
 -- 1. What are the top 5 cities by total revenue generated over the last 3 years?
 
 with pickup_cities as (
@@ -48,9 +24,52 @@ order by total_city_revenue desc;
 
 -- 2. What is the average trip duration and distance by city and vehicle type?
 
+select
+    l.city,
+    t.vehicle,
+    avg(datediff(minute,t.pickup_time,t.drop_off_time)) as avg_trip_duration,
+    round(avg(t.trip_distance),2) as avg_distance
+from trip_details as t 
+left join locations as l 
+    on t.pulocationid = l.locationid
+group by
+    l.city,
+    t.vehicle
+order by l.city;
+
 -- 3. What are the top 10 pickup and drop-off location pairs by number of trips?
 
 -- 4. What is the average surge fee by hour of day and city? Highlight peak surge periods.
+
+-- calculates average surge fee by pickup hour and city
+with surge_by_hour as(
+    select
+        pickup_city,
+        datepart(hour,pickup_time) as pickup_hour,
+        round(avg(surge_fee),2) as avg_surge
+    from td_locations
+    group by
+        pickup_city,
+        DATEPART(HOUR,pickup_time)
+
+
+
+),
+-- ranks the surcharge by pickup city from highest to lowest
+ranked as(
+    select
+        *,
+        rank() over(partition by pickup_city order by avg_surge desc) as surge_rank
+    from surge_by_hour
+
+)
+
+-- retrieves the pickup hour with the highest avg_surcharge per city
+select *
+from ranked
+where surge_rank = 1
+    and pickup_city <> 'N/A'
+order by pickup_city;
 
 -- 5. Which cities have the highest average fare per mile traveled? Exclude trips under 1 mile.
 
