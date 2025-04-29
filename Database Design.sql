fbe1de6b1446e668ab1706632c7f339f95b482ef
Gurjeevan <gurjeevanmalhi@outlook.com>
2025-04-28 20:17:27 -0500

    Updated formatting

--- Database Design.sql
+++ Database Design.sql
@@ -1,43 +1,38 @@
 -- Database Design
 
 -- Creating Trip Details Table
-
-create table trip_details(
-	trip_id int primary key,
-	pickup_time timestamp not null,
-	drop_off_time timestamp not null,
-	passenger_count smallint not null,
-	trip_distance decimal not null,
-	pu_location_id smallint not null,
-	do_location_id smallint not null,
-	fare_amount decimal not null,
-	surge_fee decimal not null,
-	vehicle varchar(20) not null,
-	payment_type varchar(20) not null
-);
+CREATE TABLE trip_details (
+	trip_id INT PRIMARY KEY,
+	pickup_time TIMESTAMP NOT NULL,
+	drop_off_time TIMESTAMP NOT NULL,
+	passenger_count SMALLINT NOT NULL,
+	trip_distance DECIMAL NOT NULL,
+	pu_location_id SMALLINT NOT NULL,
+	do_location_id SMALLINT NOT NULL,
+	fare_amount DECIMAL NOT NULL,
+	surge_fee DECIMAL NOT NULL,
+	vehicle VARCHAR(20) NOT NULL,
+	payment_type VARCHAR(20) NOT NULL
+	);
 
 -- Creating Locations Table
-
-create table locations(
-	location_id int,
-	location_name varchar(75) not null,
-	city varchar(30) not null
-);
+CREATE TABLE locations (
+	location_id INT,
+	location_name VARCHAR(75) NOT NULL,
+	city VARCHAR(30) NOT NULL
+	);
 
 -- Creating Windows View
-
-create view td_locations as
-    select
-        t.*,
-        l1.location_name as pickup_location,
-        l1.city as pickup_city,
-        l2.location_name as dropoff_location,
-        l2.city as dropoff_city
-    from trip_details as t 
-    left join locations as l1 
-        on t.pu_location_id= l1.location_id
-     left join locations as l2
-        on t.do_location_id = l2.location_id;
+CREATE VIEW td_locations
+AS
+SELECT t.*,
+	l1.location_name AS pickup_location,
+	l1.city AS pickup_city,
+	l2.location_name AS dropoff_location,
+	l2.city AS dropoff_city
+FROM trip_details AS t
+LEFT JOIN locations AS l1 ON t.pu_location_id = l1.location_id
+LEFT JOIN locations AS l2 ON t.do_location_id = l2.location_id;
 
 
 	
